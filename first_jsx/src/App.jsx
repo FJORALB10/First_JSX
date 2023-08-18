bbbd6c9f0f2b0ff3747f1840471128c03a7b193b
Fjoralb Taraj <fjoralbtaraj@gmail.com>
2023-08-18 23:29:36 +0200

    initial commit

---
+++ first_jsx/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <div className='list'>
+    <h1>Hello Dojo!</h1>
+    <h2>Things I need to do:</h2>
+    <ul className='list_style'>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+    </div>
+    </>
+  )
+}
+
+export default App
